6d7788aa52630463c3a533593a1d05dddf863baa
LucasNarZ <lucasnarloch123@gmail.com>
2026-06-13 22:21:38 -0300

    refactor: files

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# MySQL database URL
-database_url = 'mysql+pymysql://username:password@host/database_name'
-
--- frontend/js/coneção_data/paciente.js
+++
@@ -1,2 +0,0 @@
-const form = document.getElementById("meuForm");
-enviarFormulario(form, "http://localhost:3000/api/pacientes");
